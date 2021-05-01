07e59ba479c6c242285e2c43c57bcd996009bffd
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-01 21:39:25 +0530

    ShasaBot

--- ShasaBot/modules/sql_extended/forceSubscribe_sql.py
+++ ShasaBot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from YoneRobot.modules.sql import BASE, SESSION
+from ShasaBot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
--- ShasaBot/modules/sql_extended/nsfw_watch_sql.py
+++ ShasaBot/modules/sql_extended/nsfw_watch_sql.py
@@ -13,7 +13,7 @@
 
 from sqlalchemy import Column, String
 
-from YoneRobot.modules.sql import BASE, SESSION
+from ShasaBot.modules.sql import BASE, SESSION
 
 
 class Nsfwatch(BASE):
