e72325b8b766384d50509e4bdfb8d82e92f8d664
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-02 11:58:49 +0530

    Add files via upload

--- ShasaBot/modules/__telegraph.py
+++ ShasaBot/modules/__telegraph.py
@@ -6,9 +6,9 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-MdNoor = "SHASA"
+kittu = "YONE"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=mdnoor)
+r = telegraph.create_account(short_name=kittu)
 auth_url = r["auth_url"]
 
 
