8958ba0e36c85b1b022c1b384e4a06e5af4518f1
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-02 09:27:54 +0530

    Update __main__.py

--- ShasaBot/__main__.py
+++ ShasaBot/__main__.py
@@ -633,7 +633,7 @@ def donate(update: Update, context: CallbackContext):
             DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
 
-        if OWNER_ID !=  and DONATION_LINK:
+        if OWNER_ID != 1099219137 and DONATION_LINK:
             update.effective_message.reply_text(
                 "You can also donate to the person currently running me "
                 "[here]({})".format(DONATION_LINK),
