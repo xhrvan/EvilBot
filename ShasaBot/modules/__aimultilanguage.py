cb02cef2aefba2fb19c33c64cec6bddbc00ccf90
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-04 12:13:21 +0530

    Update __aimultilanguage.py

--- ShasaBot/modules/__aimultilanguage.py
+++ ShasaBot/modules/__aimultilanguage.py
@@ -183,7 +183,7 @@ async def hmm(client, message):
         if not "en" in lan and not lan == "":
             pro = translator.translate(pro, lang_tgt=lan[0])
         try:
-            await asuna.send_chat_action(message.chat.id, "typing")
+            await shasa.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
         except CFError as e:
             print(e)
