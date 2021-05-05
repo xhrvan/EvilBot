ecee3ed6b20ee5bfcf50c2ba931991db122be073
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-05 13:30:05 +0530

    Update __tools.py

--- ShasaBot/modules/__tools.py
+++ ShasaBot/modules/__tools.py
@@ -2,7 +2,7 @@ __mod_name__ = "◎Tools"
 
 __help__ = """
 *Telegraph*
- ❍ /txm :Get Telegraph Link Of Replied Media
+ ❍ /tm :Get Telegraph Link Of Replied Media
  ❍ /txt :Get Telegraph Link of Replied Text
 
 *Chatbot* 
