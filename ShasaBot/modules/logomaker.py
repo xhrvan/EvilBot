c55eed21a78523994aca6a46971eecf1984e44a6
Dipesh <83916289+xdipesh@users.noreply.github.com>
2021-05-16 12:19:51 +0530

    Update logomaker.py

--- ShasaBot/modules/logomaker.py
+++ ShasaBot/modules/logomaker.py
@@ -36,7 +36,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByShasa.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By ShasaBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By KristinaBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
