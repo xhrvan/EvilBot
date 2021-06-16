55ccf72bb1bc5372852d0794f842c7768bf0ea0d
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 14:31:07 +0530

    Update mongo.py

--- EvilBot/mongo.py
+++ EvilBot/mongo.py
@@ -3,10 +3,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from ShasaBot import MONGO_DB_URI 
+from EvilBot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from ShasaBot.conf import get_int_key, get_str_key
+from EvilBot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -18,7 +18,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["shasabot"]
+db = client["evilbot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
