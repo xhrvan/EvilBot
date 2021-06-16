1f200a45d95fa2f0a1ad61ef630b4acfad84761c
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 19:39:29 +0530

    Update approve.py

--- EvilBot/modules/approve.py
+++ EvilBot/modules/approve.py
@@ -1,11 +1,11 @@
 import html
-from ShasaBot.modules.disable import DisableAbleCommandHandler
-from ShasaBot import dispatcher, DRAGONS
-from ShasaBot.modules.helper_funcs.extraction import extract_user
+from EvilBot.modules.disable import DisableAbleCommandHandler
+from EvilBot import dispatcher, DRAGONS
+from EvilBot.modules.helper_funcs.extraction import extract_user
 from telegram.ext import CallbackContext, CallbackQueryHandler, Filters, run_async
-import ShasaBot.modules.sql.approve_sql as sql
-from ShasaBot.modules.helper_funcs.chat_status import user_admin
-from ShasaBot.modules.log_channel import loggable
+import EvilBot.modules.sql.approve_sql as sql
+from EvilBot.modules.helper_funcs.chat_status import user_admin
+from EvilBot.modules.log_channel import loggable
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
