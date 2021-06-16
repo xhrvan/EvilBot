3935fb4c69e13fee49dac79cfa67dc5eb7f06975
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:54:36 +0530

    Update feds_sql.py

--- EvilBot/modules/sql/feds_sql.py
+++ EvilBot/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from ShasaBot import dispatcher
-from ShasaBot.modules.sql import BASE, SESSION
+from EvilBot import dispatcher
+from EvilBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
