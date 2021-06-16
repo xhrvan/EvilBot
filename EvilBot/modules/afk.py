c733e5a04a50392414b770defa2906b98a6eeb00
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 19:36:00 +0530

    Update afk.py

--- EvilBot/modules/afk.py
+++ EvilBot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from ShasaBot import dispatcher
-from ShasaBot.modules.disable import (
+from EvilBot import dispatcher
+from EvilBot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from ShasaBot.modules.sql import afk_sql as sql
-from ShasaBot.modules.users import get_user_id
+from EvilBot.modules.sql import afk_sql as sql
+from EvilBot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
