4c01ce22ff514322633b1662a9877d728b22ee5c
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 14:33:30 +0530

    Update sample_config.py

--- EvilBot/sample_config.py
+++ EvilBot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/ShasaBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/EvilBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1099219137  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "MdNoor"
-    SUPPORT_CHAT = "MissShasa_Gbot"  # Your own group for support, do not add the @
+    OWNER_ID = 1742353529  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "shrvan42"
+    SUPPORT_CHAT = "EVIL_SUPPORT_CHAT"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001236063876
     )  # Prints any new group the bot is added to, prints just the name and ID.
