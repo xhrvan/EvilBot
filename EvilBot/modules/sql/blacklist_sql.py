69569557772efbef2532e24ec0f56b854a00152f
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:58:07 +0530

    Update blacklist_sql.py

--- EvilBot/modules/sql/blacklist_sql.py
+++ EvilBot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from ShasaBot.modules.sql import SESSION, BASE
+from EvilBot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
