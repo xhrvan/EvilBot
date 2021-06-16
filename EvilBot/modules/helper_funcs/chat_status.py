8193a93b4377e6d9678fec09f3eafc4a082d2ebe
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:47:10 +0530

    Update chat_status.py

--- EvilBot/modules/helper_funcs/chat_status.py
+++ EvilBot/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from ShasaBot import (
+from EvilBot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
