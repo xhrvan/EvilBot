f9149bc433e77720d68304cfed1f729e3bdc7749
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 16:01:00 +0530

    Update forceSubscribe_sql.py

--- EvilBot/modules/sql_extended/forceSubscribe_sql.py
+++ EvilBot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from ShasaBot.modules.sql import BASE, SESSION
+from EvilBot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
