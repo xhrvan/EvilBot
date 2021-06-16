d06cbaa69a42552e430675949de52afccb9163f3
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 21:22:00 +0530

    Update chatbot.py

--- EvilBot/modules/chatbot.py
+++ EvilBot/modules/chatbot.py
@@ -3,14 +3,14 @@ import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
 from time import sleep, time
 
-import ShasaBot.modules.sql.chatbot_sql as sql
+import EvilBot.modules.sql.chatbot_sql as sql
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
 from coffeehouse.lydia import LydiaAI
-from ShasaBot import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
-from ShasaBot.modules.helper_funcs.chat_status import user_admin
-from ShasaBot.modules.helper_funcs.filters import CustomFilters
-from ShasaBot.modules.log_channel import gloggable
+from EvilBot import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
+from EvilBot.modules.helper_funcs.chat_status import user_admin
+from EvilBot.modules.helper_funcs.filters import CustomFilters
+from EvilBot.modules.log_channel import gloggable
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import (
