0f39fb34b75c1a0431bf384d42804571de30296c
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 16:09:42 +0530

    Update __heroku.py

--- EvilBot/modules/__heroku.py
+++ EvilBot/modules/__heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from ShasaBot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from ShasaBot.events import register
+from EvilBot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from EvilBot.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
