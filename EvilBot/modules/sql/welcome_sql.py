f1e19fcbe0142196d432701dbe9cd808076f809c
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:48:59 +0530

    Update welcome_sql.py

--- EvilBot/modules/sql/welcome_sql.py
+++ EvilBot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from ShasaBot.modules.helper_funcs.msg_types import Types
-from ShasaBot.modules.sql import BASE, SESSION
+from EvilBot.modules.helper_funcs.msg_types import Types
+from EvilBot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
