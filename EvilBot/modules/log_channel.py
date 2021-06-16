771f777b3fd31fb51077de0b19f8300b4d26a9f6
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:08:34 +0530

    Update log_channel.py

--- EvilBot/modules/log_channel.py
+++ EvilBot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from ShasaBot.modules.helper_funcs.misc import is_module_loaded
+from EvilBot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from ShasaBot import EVENT_LOGS, LOGGER, dispatcher
-    from ShasaBot.modules.helper_funcs.chat_status import user_admin
-    from ShasaBot.modules.sql import log_channel_sql as sql
+    from EvilBot import EVENT_LOGS, LOGGER, dispatcher
+    from EvilBot.modules.helper_funcs.chat_status import user_admin
+    from EvilBot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
