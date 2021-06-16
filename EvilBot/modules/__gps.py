79f8f4642d49466ab640a4c1e3068fc884478671
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 16:09:10 +0530

    Update __gps.py

--- EvilBot/modules/__gps.py
+++ EvilBot/modules/__gps.py
@@ -1,8 +1,8 @@
 import os
-from ShasaBot import telethn as tbot
+from EvilBot import telethn as tbot
 from geopy.geocoders import Nominatim
-from ShasaBot.events import register
-from ShasaBot import *
+from EvilBot.events import register
+from EvilBot import *
 from telethon import *
 from telethon.tl import *
 
