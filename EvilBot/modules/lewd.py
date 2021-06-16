e1bf426dc7d4962b70f6b5dd5173308459b2a6b5
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:06:43 +0530

    Update lewd.py

--- EvilBot/modules/lewd.py
+++ EvilBot/modules/lewd.py
@@ -7,7 +7,7 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async
 
-from ShasaBot import dispatcher, updater
+from EvilBot import dispatcher, updater
 
 
 def is_user_in_chat(chat: Chat, user_id: int) -> bool:
