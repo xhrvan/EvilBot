800f277b03f7e280ae5f87c97fc37d05319bc849
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:42:13 +0530

    Update msg_types.py

--- EvilBot/modules/helper_funcs/msg_types.py
+++ EvilBot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from ShasaBot.modules.helper_funcs.string_handling import button_markdown_parser
+from EvilBot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
