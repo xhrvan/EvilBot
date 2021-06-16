ebba6087e18f2d58f2e6e774c5a829efe1c58241
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 16:07:05 +0530

    Update __english.py

--- EvilBot/modules/__english.py
+++ EvilBot/modules/__english.py
@@ -1,9 +1,9 @@
-from ShasaBot import telethn as tbot
+from EvilBot import telethn as tbot
 import json
 import os
 os.system("pip installPyDictionary")
 import requests
-from ShasaBot.events import register
+from EvilBot.events import register
 from telethon import *
 from telethon.tl import functions
 import os
