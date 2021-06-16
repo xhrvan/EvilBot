3e3a9d5b1164cc793911dae37391a4abab463fa4
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:20:43 +0530

    Update shell.py

--- EvilBot/modules/shell.py
+++ EvilBot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from ShasaBot import LOGGER, dispatcher
-from ShasaBot.modules.helper_funcs.chat_status import dev_plus
+from EvilBot import LOGGER, dispatcher
+from EvilBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
