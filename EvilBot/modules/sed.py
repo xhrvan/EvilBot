a4c97a86a15394d13ef6580767988caaa0921bb6
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:20:18 +0530

    Update sed.py

--- EvilBot/modules/sed.py
+++ EvilBot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from ShasaBot import LOGGER, dispatcher
-from ShasaBot.modules.disable import DisableAbleMessageHandler
-from ShasaBot.modules.helper_funcs.regex_helper import infinite_loop_check
+from EvilBot import LOGGER, dispatcher
+from EvilBot.modules.disable import DisableAbleMessageHandler
+from EvilBot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
