54375c1de5ff1c4d5759dfe6f0d0c51f8c2b6c53
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 19:36:55 +0530

    Update anime.py

--- EvilBot/modules/anime.py
+++ EvilBot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from ShasaBot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from ShasaBot.modules.disable import DisableAbleCommandHandler
+from EvilBot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from EvilBot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
