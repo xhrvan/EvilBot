a4f55c82ec0e5ee6010d1a52b25dd59fc3d8b8f8
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 13:44:51 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Miss Shasa
-:: Enables virtual env mode and then starts Shasa
-env\scripts\activate.bat && py -m ShasaBott
+TITLE Evil Xobot 
+:: Enables virtual env mode and then starts Evil
+env\scripts\activate.bat && py -m EvilBott
