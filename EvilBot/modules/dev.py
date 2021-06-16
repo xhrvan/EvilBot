7089be0fd08d97912f1c755825a23fdb4120a2a2
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 21:55:12 +0530

    Update dev.py

--- EvilBot/modules/dev.py
+++ EvilBot/modules/dev.py
@@ -21,9 +21,9 @@ def allow_groups(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f"Current state: {ShasaBot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
-        ShasaBot.ALLOW_CHATS = True
+        EvilBot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        ShasaBot.ALLOW_CHATS = False
+        EvilBot.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
