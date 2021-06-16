ab4a43afe1eae0c5b034f422192ba3105a2d2d88
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 16:06:01 +0530

    Update __country.py

--- EvilBot/modules/__country.py
+++ EvilBot/modules/__country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from ShasaBot import telethn as borg
-from ShasaBot.events import register
+from EvilBot import telethn as borg
+from EvilBot.events import register
 
 
 @register(pattern="^/country (.*)")
