5e610025117218dbee88401ca59d99ec205f1b46
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:16:58 +0530

    Update purge.py

--- EvilBot/modules/purge.py
+++ EvilBot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from ShasaBot import telethn
-from ShasaBot.modules.helper_funcs.telethn.chatstatus import (
+from EvilBot import telethn
+from EvilBot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
