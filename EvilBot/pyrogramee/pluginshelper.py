d0e54a682631e4723c1cc32fa771159f5696529a
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 14:39:51 +0530

    Update pluginshelper.py

--- EvilBot/pyrogramee/pluginshelper.py
+++ EvilBot/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from ShasaBot  import OWNER_ID, SUPPORT_CHAT
-from ShasaBot import pbot
+from EvilBot  import OWNER_ID, SUPPORT_CHAT
+from EvilBot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
