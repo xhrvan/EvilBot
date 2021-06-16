0f2b492aa6a246b3f143701ac942b52a597ddbde
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 21:32:17 +0530

    Update disasters.py

--- EvilBot/modules/disasters.py
+++ EvilBot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from ShasaBot import (
+from EvilBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from ShasaBot import (
     WOLVES,
     dispatcher,
 )
-from ShasaBot.modules.helper_funcs.chat_status import (
+from EvilBot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from ShasaBot.modules.helper_funcs.extraction import extract_user
-from ShasaBot.modules.log_channel import gloggable
+from EvilBot.modules.helper_funcs.extraction import extract_user
+from EvilBot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
