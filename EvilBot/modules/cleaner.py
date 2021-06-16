4534c0c97c0ee08451735a0904ea86c909339d97
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 21:22:41 +0530

    Update cleaner.py

--- EvilBot/modules/cleaner.py
+++ EvilBot/modules/cleaner.py
@@ -1,15 +1,15 @@
 
 import html
 
-from ShasaBot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from ShasaBot.modules.disable import DisableAbleCommandHandler
-from ShasaBot.modules.helper_funcs.chat_status import (
+from EvilBot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from EvilBot.modules.disable import DisableAbleCommandHandler
+from EvilBot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from ShasaBot.modules.sql import cleaner_sql as sql
+from EvilBot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
