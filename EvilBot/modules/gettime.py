0fcc432fe16381b34c032bf3c98bcca72ebf6bf6
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 22:00:28 +0530

    Update gettime.py

--- EvilBot/modules/gettime.py
+++ EvilBot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from ShasaBot import TIME_API_KEY, dispatcher
-from ShasaBot.modules.disable import DisableAbleCommandHandler
+from EvilBot import TIME_API_KEY, dispatcher
+from EvilBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
