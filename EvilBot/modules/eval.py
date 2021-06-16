528172206530f8980b855f5f7339c788a4f442b1
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 21:34:33 +0530

    Update eval.py

--- EvilBot/modules/eval.py
+++ EvilBot/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from ShasaBot import LOGGER, dispatcher
-from ShasaBot.modules.helper_funcs.chat_status import dev_plus
+from EvilBot import LOGGER, dispatcher
+from EvilBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
