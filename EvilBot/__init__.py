f1c52a285eee5336e859799d007b68e4f71dbc9e
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 14:20:16 +0530

    Update __init__.py

--- EvilBot/__init__.py
+++ EvilBot/__init__.py
@@ -102,7 +102,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from ShasaBot.config import Development as Config
+    from EvilBot.config import Development as Config
 
     TOKEN = Config.TOKEN
 
