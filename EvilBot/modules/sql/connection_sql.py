d571cfac87ee86355f49d46bb0e5007a3c4a9200
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-16 15:56:03 +0530

    Update connection_sql.py

--- EvilBot/modules/sql/connection_sql.py
+++ EvilBot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from ShasaBot.modules.sql import SESSION, BASE
+from EvilBot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
