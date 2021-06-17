a351099f0711bae973f5321a4fd55c5a9a8d2664
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-17 15:06:52 +0530

    Update chat_status.py

--- EvilBot/modules/helper_funcs/chat_status.py
+++ EvilBot/modules/helper_funcs/chat_status.py
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from ShasaBot.modules import connection
+from EvilBot.modules import connection
 
 connected = connection.connected
