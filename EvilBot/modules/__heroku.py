0e675e1085526bc7aedd03e9db14a8c4a4781777
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-17 14:57:07 +0530

    Update __heroku.py

--- EvilBot/modules/__heroku.py
+++ EvilBot/modules/__heroku.py
@@ -191,7 +191,7 @@ async def _(dyno):
         dyno.chat_id,
         "logs.txt",
         reply_to=dyno.id,
-        caption="shasa Bot Logz.",
+        caption="evil Bot Logz.",
     )
 
     await asyncio.sleep(5)
