8bedd4deeba62c93baf906b02d0aee9e1484baec
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-17 11:09:49 +0530

    Update __google.py

--- EvilBot/modules/__google.py
+++ EvilBot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Shasa <==="
+        app_details += "\n\n===> Evil <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Evil <query>*:* Shasa answers the query
-  💡Ex: `Shasa where is India?`
+ ❍ Evil <query>*:* Evil answers the query
+  💡Ex: `Evil where is India?`
 """
