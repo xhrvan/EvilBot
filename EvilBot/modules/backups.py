b7d10ff45eb560687fee4f4302f3988329651354
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-17 11:11:39 +0530

    Update backups.py

--- EvilBot/modules/backups.py
+++ EvilBot/modules/backups.py
@@ -341,7 +341,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("ShasaBot{}.backup".format(chat_id), "rb"),
+        document=open("EvilBot{}.backup".format(chat_id), "rb"),
         caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `ShasaBot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
@@ -349,7 +349,7 @@ def export_data(update, context):
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("ShasaBot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("EvilBot{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
