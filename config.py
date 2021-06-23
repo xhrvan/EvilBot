8150b759ed720ba4f13eee71a930d91003e6cff9
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-23 20:48:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,3 @@ admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
