6a3cef1aedcde6f395287928ba66b807d1ceef85
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-23 19:23:03 +0530

    Update mongo.py

--- EvilBot/mongo.py
+++ EvilBot/mongo.py
@@ -11,7 +11,7 @@ from EvilBot.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "Evil"
 
 
 client = MongoClient()
