54d0b85200bf29c0d99664c91874919286b6dc8f
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-27 18:50:46 +0530

    Update logomaker.py

--- EvilBot/modules/logomaker.py
+++ EvilBot/modules/logomaker.py
@@ -36,7 +36,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByEvil.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By EvilBot")
+    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -76,7 +76,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByEvil.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By EvilBot")
+    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
